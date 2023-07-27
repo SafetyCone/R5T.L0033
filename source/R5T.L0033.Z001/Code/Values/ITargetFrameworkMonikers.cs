e886f97a99db6cd88d4a13f0ddad98002a39a9cb
davidcoats <david@getsafetycone.com>
2023-07-27 01:31:11 -0700

    Interim changes.

--- source/R5T.L0033.Z001/Code/Values/ITargetFrameworkMonikers.cs
+++ source/R5T.L0033.Z001/Code/Values/ITargetFrameworkMonikers.cs
@@ -1,7 +1,7 @@
 using System;
 
-using R5T.L0032.T000;
 using R5T.T0131;
+using R5T.T0218;
 
 
 namespace R5T.L0033.Z001
@@ -9,20 +9,20 @@ namespace R5T.L0033.Z001
     [ValuesMarker]
     public partial interface ITargetFrameworkMonikers : IValuesMarker
     {
-        public static L0032.Z000.ITargetFrameworkMonikers Base => L0032.Z000.TargetFrameworkMonikers.Instance;
+        public static Z0057.ITargetFrameworkMonikers Base => Z0057.TargetFrameworkMonikers.Instance;
 
 
-        /// <inheritdoc cref="L0032.Z000.ITargetFrameworkMonikers.NET_6"/>
+        /// <inheritdoc cref="Z0057.ITargetFrameworkMonikers.NET_6"/>
         public ITargetFrameworkMoniker NET_6 => Base.NET_6;
 
 
         /// <summary>
         /// The default target framework moniker for console projects.
-        /// (<inheritdoc cref="L0032.Z000.ITargetFrameworkMonikers.NET_6" path="/summary"/>)
+        /// (<inheritdoc cref="Z0057.ITargetFrameworkMonikers.NET_6" path="/summary"/>)
         /// </summary>
         public ITargetFrameworkMoniker Console => Base.NET_6;
 
-        /// <inheritdoc cref="L0032.Z000.ITargetFrameworkMonikers.NET_6"/>
+        /// <inheritdoc cref="Z0057.ITargetFrameworkMonikers.NET_6"/>
         public ITargetFrameworkMoniker Library => Base.NET_6;
     }
 }
